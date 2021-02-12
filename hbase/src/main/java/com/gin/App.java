f0306718f2939cbd23c7a06248d48499f105378d
gin <yuonuotianming@163.com>
2021-02-12 17:10:01 +0800

    hbase

---
+++ hbase/src/main/java/com/gin/App.java
@@ -0,0 +1,13 @@
+package com.gin;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
