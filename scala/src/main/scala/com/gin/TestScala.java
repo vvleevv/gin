3c2e77dcb28085ddc57782824bcd04257e442f67
gin <yuonuotianming@163.com>
2021-02-19 09:49:26 +0800

    deal pom

---
+++ scala/src/main/scala/com/gin/TestScala.java
@@ -0,0 +1,13 @@
+package com.gin;
+
+/**
+ * Hello world!
+ *
+ */
+public class TestScala
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
